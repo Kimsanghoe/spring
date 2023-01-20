08116febb9ea4cce7ca4ebdc2b1795a0cc0de89e
Kimsanghoe <mace302@naver.com>
2023-01-20 17:40:58 +0900

    로그인 처리2 - 필터, 인터셉터 */
    20230120

---
+++ mvc_2/login/src/main/java/hello/login/web/filter/LogFilter.java
@@ -0,0 +1,43 @@
+package hello.login.web.filter;
+
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.UUID;
+
+@Slf4j
+public class LogFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        log.info("log filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        log.info("log filter doFilter");
+
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String requestURI = httpRequest.getRequestURI();
+
+        String uuid = UUID.randomUUID().toString();
+
+        //같은 요청의 로그에 모든 같은 식별자를 남기고 싶으면 logback mdc를 참고해보자
+
+        try {
+            log.info("REQUEST [{}][{}]", uuid, requestURI);
+            chain.doFilter(request, response);
+        } catch (Exception e) {
+            throw e;
+        }finally {
+            log.info("RESPONSE [{}][{}]", uuid, requestURI);
+        }
+
+    }
+
+    @Override
+    public void destroy() {
+        log.info("log filter destroy");
+    }
+}
