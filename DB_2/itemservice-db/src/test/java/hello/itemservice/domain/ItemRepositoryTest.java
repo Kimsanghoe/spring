5be60d7f196897a37ebb26e8524cb69bc11fff86
Kimsanghoe <mace302@naver.com>
2023-02-01 19:00:09 +0900

    데이터 접근 기술 - 테스트 */

--- DB_2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ DB_2/itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -8,16 +8,29 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+/*    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+
+    @BeforeEach
+    void beforeEach() {
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }*/
 
     @AfterEach
     void afterEach() {
@@ -25,8 +38,13 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        //트랜잭션 롤백
+//        transactionManager.rollback(status);
+
     }
 
+//    @Commit           /*데이터베이스에서 눈으로 확인하고 싶을경우*/
+//    @Transactional
     @Test
     void save() {
         //given
