acf2b64eb398999918a421a92b6aef080149ac2a
Kimsanghoe <mace302@naver.com>
2023-02-08 19:56:49 +0900

    스프링 트랜잭션 이해 */
    20230208

---
+++ DB_2/springtx/src/test/java/kim/springtx/apply/InitTxTest.java
@@ -0,0 +1,55 @@
+package kim.springtx.apply;
+
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@SpringBootTest
+class InitTxTest {
+
+    @Autowired
+    Hello hello;
+
+    @Test
+    void go() {
+        //초기화 시점에 자동으로 @PostConstruct는 실행
+    }
+
+
+    @TestConfiguration
+    static class InitTxTestConfig{
+        @Bean
+        Hello hello() {
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello {
+
+        @PostConstruct
+        @Transactional
+        public void initV1() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruct tx active={}", isActive);
+        }
+
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
+        public void initV2() {
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @ApplicationReadyEvent tx active={}", isActive);
+        }
+
+    }
+
+
+}
